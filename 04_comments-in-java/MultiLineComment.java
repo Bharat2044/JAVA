192cfabb949a9f4262bc293b183da3c3de5776b6
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-09-02 07:18:24 +0000

    code added

---
+++ 04_comments-in-java/MultiLineComment.java
@@ -0,0 +1,12 @@
+public class SingleLineComment {
+        
+    public static void main(String args[]) {
+        
+        System.out.println("Multi Line Comments");
+        
+        /* This is 
+        multi 
+        line 
+        comments */
+    }
+}
